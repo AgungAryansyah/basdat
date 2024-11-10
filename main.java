168a69e5ef617f5f7abae6f33a562e105408f4dd
“AgungAryansyah” <agungaryansyah2006@gmail.com>
2024-11-10 14:36:49 +0700

    test

--- main.java
+++ main.java
@@ -1,8 +1,5 @@
 public class main {
   public static void main(String[] args) {
-        System.out.println("testtest");
-        int i = 1;
-        System.out.println(i);
-
+        System.out.println("test");
     }
 }
